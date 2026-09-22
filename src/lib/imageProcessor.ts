ef8b42da70a5bae120c4f4331dd529ebd5ce7d6a
Aalyan Rasheed <mianaalyan28@gmail.com>
2026-09-22 20:38:22 +0500

    cleanup

--- src/lib/imageProcessor.ts
+++
@@ -1,8 +0,0 @@
-import { getWorkerPool } from './workerPool';
-export * from './conversionOrchestrator';
-export * from './workerPool';
-export * from './codecs';
-
-export function terminateWorkers() {
-  getWorkerPool().terminateAll();
-}
